567398e6de6c619b4e6a1a043505256e0828e50c
Brock Lanoza <brock.lanoza@hpe.com>
2016-10-15 17:10:20 -1000

    Add buttons to header for selecting offensive or defensive stats

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { nflTeams } from './constants';
 import { TeamFantasyPts, CbStrength } from './components';
 import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
 import './App.css';
-import { nflTeams } from './constants';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      defenseColor: '#039be5',
+      defenseLabelColor: '#f2f2f2',
+      offenseColor: '#fff',
+      offenseLabelColor: '#000',
+      offOrDef: 'def'
+    };
+  }
   render() {
     const teamStats = nflTeams.map(({ team, logo }, i) => {
       if (i === 0 || i % 3 === 0) {
         return (
-          <div className="row">
+          <div className="row" key={i}>
             <TeamFantasyPts 
               key={team}
+              offOrDef={this.state.offOrDef}
               avatar={logo} 
               team={team} 
               seas={2016}
@@ -23,6 +35,7 @@ class App extends Component {
       return (
       <TeamFantasyPts 
         key={team} 
+        offOrDef={this.state.offOrDef}
         team={team}
         avatar={logo} 
         seas={2016}
@@ -40,6 +53,22 @@ class App extends Component {
       <div className="container">
         <div className="App-header">
           <AppBar title="Better Odds of Football" />
+          <div className="row buttons">
+            <div className="six columns">
+              <RaisedButton 
+                label="Offense" 
+                labelColor={this.state.offenseLabelColor}
+                backgroundColor={this.state.offenseColor}
+              />
+            </div>
+            <div className="six columns">
+              <RaisedButton 
+                label="Defense" 
+                labelColor={this.state.defenseLabelColor}
+                backgroundColor={this.state.defenseColor}
+              />
+            </div>
+          </div>
         </div>
         <div className="cards">
           {teamStats}
